c33c4b76e81401a43cd9cc28c838b15e3613d32a
dartw <dartwadar@yandex.ru>
2024-06-20 22:17:11 +0500

    Test.

--- BookStore.Core/Abstractions/IBooksRepository.cs
+++ BookStore.Core/Abstractions/IBooksRepository.cs
@@ -5,7 +5,7 @@ namespace BookStore.Core.Abstractions;
 public interface IBooksRepository
 {
     Task<Guid> Create(Book book);
-    Task<List<Book>> Get();
     Task<Guid> Update(Guid id, string title, string description, decimal price);
+    Task<List<Book>> Get();
     Task<Guid> Delete(Guid id);
 }
