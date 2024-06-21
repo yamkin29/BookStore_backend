52bdbeb715cf6c16acb3e520aaa6fe73a9274662
yamkin29 <me@jamkin.ru>
2024-06-21 22:25:30 +0500

    Added connection to PostgresSQL.

--- BookStore.API/Program.cs
+++ BookStore.API/Program.cs
@@ -1,15 +1,20 @@
+using BookStore.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<BookStoreDbContext>(
+    options =>
+    {
+        options.UseNpgsql(builder.Configuration.GetConnectionString(nameof(BookStoreDbContext)));
+    });
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
---
+++ BookStore.Application/Services/BooksService.cs
@@ -0,0 +1,6 @@
+﻿namespace BookStore.Application.Services;
+
+public class BooksService
+{
+    
+}
