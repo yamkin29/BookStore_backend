84c5bd4369344a19b048a455b1ba98b07c101883
yamkin29 <me@jamkin.ru>
2024-06-21 23:19:15 +0500

    Add some controllers and completed migrations.

---
+++ BookStore.API/Contracts/BooksResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace BookStore.Contracts;
+
+public record BooksResponse (
+    Guid Id,
+    string Title,
+    string Description,
+    decimal Price);
---
+++ BookStore.API/Controllers/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using BookStore.Contracts;
+using BookStore.Core.Abstractions;
+using BookStore.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BooksController : ControllerBase
+{
+    private readonly IBooksService _booksService;
+
+    public BooksController(IBooksService booksService)
+    {
+        _booksService = booksService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<BooksResponse>>> GetBooks()
+    {
+        var books = await _booksService.GetAllBooks();
+        
+        var response  = books.Select(x => new BooksResponse(x.Id, x.Title, x.Description, x.Price));
+        
+        return Ok(response);
+    }
+}
