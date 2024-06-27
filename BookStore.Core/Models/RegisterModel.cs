5baf5ee267516981b0b9895f4bebc23c3c8b9301
yamkin29 <me@jamkin.ru>
2024-06-27 22:10:18 +0500

    Added business models.

---
+++ BookStore.Core/Models/RegisterModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BookStore.Core.Models;
+
+public class RegisterModel
+{
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+    public string FirstName { get; set; }
+    
+    public string LastName  { get; set; }
+}
--- BookStore.DataAccess/Entities/UserEntity.cs
+++ BookStore.DataAccess/Entities/UserEntity.cs
@@ -4,5 +4,9 @@ namespace BookStore.DataAccess.Entities;
 
 public class UserEntity : IdentityUser
 {
+    public string FirstName { get; set; }
     
+    public string LastName { get; set; }
+    
+    public DateTime DateOfBirth { get; set; }
 }
