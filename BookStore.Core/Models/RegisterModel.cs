83db060bdae881da209d6f81f34f861ff81bb5e8
yamkin29 <me@jamkin.ru>
2024-06-30 21:27:41 +0500

    Add RegisterModel.cs.

--- BookStore.Core/Models/RegisterModel.cs
+++ BookStore.Core/Models/RegisterModel.cs
@@ -2,6 +2,8 @@
 
 public class RegisterModel
 {
+    public const int MAX_EMAIL_LENGTH = 250;
+    
     public string Email { get; set; }
 
     public string Password { get; set; }
@@ -9,4 +11,26 @@ public class RegisterModel
     public string FirstName { get; set; }
     
     public string LastName  { get; set; }
+
+    public RegisterModel(string email, string password, string firstName, string lastName)
+    {
+        Email = email;
+        Password = password;
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public static (RegisterModel registerModel, string error) Create(string email, string password, string firstName, string lastName)
+    {
+        var error = string.Empty;
+
+        if (string.IsNullOrEmpty(email) || email.Length  > MAX_EMAIL_LENGTH)
+        {
+            error  = "Title cannot be null or empty";
+        }
+        
+        var registerModel  = new RegisterModel(email, password, firstName, lastName);
+        
+        return (registerModel, error);
+    }
 }
---
+++ BookStore.DataAccess/Configurations/UserConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Core.Models;
+using BookStore.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookStore.DataAccess.Configurations;
+
+public class UserConfiguration
+{
+    public void Configure(EntityTypeBuilder<UserEntity> builder)
+    {
+        
+    }
+}
