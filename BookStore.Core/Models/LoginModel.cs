f52e48a1c213e2de093a46134d25c2d20537cbed
yamkin29 <me@jamkin.ru>
2024-07-01 22:52:51 +0500

    Add ctor LoginModel.cs

--- BookStore.Core/Models/LoginModel.cs
+++ BookStore.Core/Models/LoginModel.cs
@@ -5,4 +5,10 @@ public class LoginModel
     public string Email { get; set; }
 
     public string Password { get; set; }
+
+    public LoginModel(string email, string password)
+    {
+        Email = email;
+        Password = password;
+    }
 }
